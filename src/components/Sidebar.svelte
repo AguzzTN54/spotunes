b9fc9c08ee2fa4d9601d1d7291668327b234c547
AguzzTN54 <gusagusnanang@gmail.com>
2020-12-30 20:45:53 +0700

    Animate Sidebar Collapsible

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { fade } from 'svelte/transition';
   import { isActive, url } from '@roxi/routify';
   import Icon from './Icon.svelte';
 
@@ -57,27 +58,36 @@
 
 <style>
   .sidebar {
-    @apply h-full w-14 pt-3 pb-3 bg-white bg-opacity-50;
+    @apply h-full w-14 pt-3 pb-3 bg-white bg-opacity-50 transition-all duration-500;
     backdrop-filter: blur(10px);
     -webkit-backdrop-filter: blur(10px);
   }
   .sidebar.active {
     @apply w-60;
   }
-  .hide_text {
-    @apply hidden sm:inline-block;
+  .sidebar.active .hide_text {
+    @apply inline-block;
   }
+  .sidebar.active .nav-header {
+    @apply mb-0;
+  }
+  .sidebar.active .avatar {
+    @apply ml-4 pr-0 w-12 h-12;
+  }
+  .sidebar.active .nav-body a {
+    @apply pl-6 pr-6;
+  }
+
+  /* Mini Sidebar */
   .nav-header {
-    @apply flex items-center font-medium whitespace-nowrap mb-4;
-    @apply sm:mb-0;
+    @apply flex items-center font-medium whitespace-nowrap mb-4  transition-all duration-500;
     border-bottom-width: 1px;
   }
   .nav-header .hide_text {
     @apply pr-6;
   }
   .avatar {
-    @apply w-9 h-9 mr-2 ml-2 pr-2 rounded-full bg-gray-300 object-cover;
-    @apply sm:ml-4 sm:pr-0 sm:w-12 sm:h-12;
+    @apply w-9 h-9 mr-2 ml-2 pr-2 rounded-full bg-gray-300 object-cover transition-all duration-500;
   }
   .pro {
     @apply border-pink-600 text-pink-600 pr-1 pl-1 inline-block text-center rounded-md;
@@ -85,7 +95,7 @@
     border-width: 1px;
   }
   .navTitle {
-    @apply mt-8 pr-6 pl-6 uppercase font-semibold;
+    @apply mt-8 pr-6 pl-6 uppercase font-semibold whitespace-nowrap;
   }
   a,
   ul,
@@ -94,8 +104,7 @@
   }
 
   .nav-body a {
-    @apply sm:pl-8 sm:pr-8;
-    @apply pt-2 pb-2 pr-4 pl-4 align-middle text-gray-800;
+    @apply pt-2 pb-2 pr-4 pl-4 align-middle text-gray-800 whitespace-nowrap transition-all duration-500;
     text-decoration: none;
   }
   .nav-body a:hover {
@@ -112,20 +121,42 @@
 <div class="sidebar{isSidebarExpand ? ' active' : ''}">
   <div class="nav-header">
     <img class="avatar" src="favicon.png" alt="Avatar" />
-    <div class="hide_text">
-      <a href="#/">Agustinus Yohannes</a>
-      <span class="pro">Pro</span>
-    </div>
+    {#if isSidebarExpand}
+      <div
+        class="hide_text"
+        in:fade="{{ duration: 500 }}"
+        out:fade="{{ duration: 500 }}"
+      >
+        <a href="#/">Agustinus Yohannes</a>
+        <span class="pro">Pro</span>
+      </div>
+    {/if}
   </div>
 
   {#each navLinks as { id, items, name } (id)}
-    <div class="navTitle hide_text">{name}</div>
+    {#if isSidebarExpand}
+      <div
+        class="navTitle hide_text"
+        in:fade="{{ duration: 500 }}"
+        out:fade="{{ duration: 500 }}"
+      >
+        {name}
+      </div>
+    {/if}
+
     <ul class="nav-body">
       {#each items as { title, link, icon } (title)}
         <li>
           <a href="{$url(link)}" class:active="{$isActive(link)}">
             <Icon bi="{icon}" />
-            <span class="hide_text">{title} </span>
+            {#if isSidebarExpand}
+              <span
+                class="hide_text"
+                in:fade="{{ duration: 500 }}"
+                out:fade="{{ duration: 500 }}"
+              >{title}
+              </span>
+            {/if}
           </a>
         </li>
       {/each}
